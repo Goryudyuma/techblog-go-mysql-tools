54313f6d4f745246ee089ba901cdfb0d3a2f5a06
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2025-10-28 06:15:23 +0900

    itemテーブル追加

---
+++ migrations/20251028061400-item.sql
@@ -0,0 +1,10 @@
+-- +migrate Up
+CREATE TABLE `items`
+(
+    `id` varchar(36) COLLATE utf8mb4_0900_bin NOT NULL COMMENT 'itemID',
+    `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_bin;
